edcbf1f531eea8426385ea943b449b4088c1f5c9
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-02-11 15:52:18 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
