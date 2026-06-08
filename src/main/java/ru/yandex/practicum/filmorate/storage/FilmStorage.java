da0b9f59637cc798aa64ecf5c55db7ee0e4100cf
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-06-08 15:27:13 +0300

    fix: unused import

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.FilmGenre;
 
 import java.util.Collection;
 import java.util.Set;
